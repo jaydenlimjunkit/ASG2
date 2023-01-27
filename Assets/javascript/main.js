0973f423d4a8f4a40a730520ec2a2fe05ebc34bd
jaydenlimjunkit& 'c:\Users\jayde\OneDrive\Desktop\id asg 1\ASG1\index.html <s10239986@connect.edu.np.sg>
2023-01-27 15:58:00 +0800

    apply restdb api key

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -1,1 +1,18 @@
-const APIKEY = "63d376d73bc6b255ed0c4353";
+var jsondata = {"field1": "xyz","field2": "abc"};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://idasg2-b34c.restdb.io/rest/contact",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "63d376d73bc6b255ed0c4353",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+})
