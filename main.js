473dbd9c77fae805acc30ffc02b057cc1b196a57
jaydenlimjunkit& 'c:\Users\jayde\OneDrive\Desktop\id asg 1\ASG1\index.html <s10239986@connect.edu.np.sg>
2023-02-09 15:52:52 +0800

    fixed main js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
     }
 }
 
-const header = doccument.querySelector("header");
+const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
   header.classList.toggle ("sticky", this.window.scrollY > 0)
