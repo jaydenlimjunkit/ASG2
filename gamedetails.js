86ed484e673584392d6bb8fffd004b2fcce988ee
jaydenlimjunkit& 'c:\Users\jayde\OneDrive\Desktop\id asg 1\ASG1\index.html <s10239986@connect.edu.np.sg>
2023-02-09 17:20:56 +0800

    finished wow and ow localstorage

--- gamedetails.js
+++ gamedetails.js
@@ -2,6 +2,13 @@ const mc = document.getElementById("mc");
 const mcprice = document.getElementById("mcprice");
 const mcaddtocart = document.getElementById("mcaddtocart");
 
+const ow = document.getElementById("ow");
+const owprice = document.getElementById("owprice");
+const owaddtocart = document.getElementById("owaddtocart");
+
+const wow = document.getElementById("wow");
+const wowprice = document.getElementById("wowprice");
+const wowaddtocart = document.getElementById("wowaddtocart");
 
 const IsOutput = document.getElementById("IsOutput");
 let total = 0
@@ -16,7 +23,29 @@ try{
     };
 }
 catch{
+};
+try {
+    owaddtocart.onclick = function (){
+        const overwatch = ow.textContent;
+        const overwatchprice = owprice.textContent;
+    
+        localStorage.setItem(overwatch, overwatchprice);
+    
+    };
 }
+catch{};
+try{
+    wowaddtocart.onclick = function (){
+        const worldofwarcraft = wow.textContent;
+        const worldowarcraftprice = wowprice.textContent;
+
+        localStorage.setItem(worldofwarcraft, worldowarcraftprice);
+
+    };
+}
+catch{
+};
+
 
 try{
     for (let i = 0; i < localStorage.length; i++){
