e69b566962fe96cf38dd3dc52eaff2ce4b047afa
Klayton <S10242375@connect.np.edu.sg>
2023-02-10 18:16:24 +0800

    remove unness line

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 
-  function searchbutton (){
-    let input = document.getElementById('searchright').value
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('product');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
-        }
-    }
-}
+
 
 const header = document.querySelector("header");
 
