d2224899a4b49e3147b7b0e2cb65d277f298456a
Klayton <S10242375@connect.np.edu.sg>
2023-02-10 17:03:33 +0800

    finalised review page

--- loginpage.js
+++ loginpage.js
@@ -64,3 +64,24 @@ $(document).ready(function () {
  
 
   })
+
+
+  //dropdown function for review page
+  $(function() {
+    var list = $('.js-dropdown-list');
+    var link = $('.js-link');
+    link.click(function(e) {
+      e.preventDefault();
+      list.slideToggle(200);
+    });
+    list.find('li').click(function() {
+      var text = $(this).html();
+      var icon = '<i class="fa fa-chevron-down"></i>';
+      link.html(text+icon);
+      list.slideToggle(200);
+      if (text === '* Reset') {
+        link.html('Select one option'+icon);
+      }
+    });
+  });
+
